1e8779a00ff6a4768b07d8671095df43fd533d92
restorer <restorer@zame-dev.org>
2017-04-18 20:26:17 +0300

    Version bump

--- submit-to-haxelib.sh
+++ submit-to-haxelib.sh
@@ -3,12 +3,16 @@
 pushd `dirname "$0"`
 
 [ -e zame-particles.zip ] && rm zame-particles.zip
-[ -e samples/luxeengine/bin ] && rm -r samples/luxeengine/bin
-[ -e samples/minimal/export ] && rm -r samples/minimal/export
-[ -e samples/showcase/export ] && rm -r samples/showcase/export
-[ -e samples/stresstest/export ] && rm -r samples/stresstest/export
 
-zip -r -9 zame-particles.zip * -x submit-to-haxelib.sh
+zip -r -9 zame-particles.zip * \
+    -x 'samples/luxeengine/bin/*' \
+    -x 'samples/minimal/export/*' \
+    -x 'samples/rendertotexture/export/*' \
+    -x 'samples/showcase/export/*' \
+    -x 'samples/stresstest/export/*' \
+    -x 'todo-stage3d/*' \
+    -x submit-to-haxelib.sh \
+    -x _TODO.md \
 
 [ -e zame-particles.zip ] && haxelib submit zame-particles.zip
 [ -e zame-particles.zip ] && rm zame-particles.zip
